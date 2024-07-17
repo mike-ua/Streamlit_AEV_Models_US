338ddc916191df34c47e9000f353cc3f017d901a
Mike <88722598+mike-ua@users.noreply.github.com>
2024-07-17 13:53:30 +0300

    Update AEV_models.py
    changed from two columns to one

--- AEV_models.py
+++ AEV_models.py
@@ -8,31 +8,31 @@ st.write('Light Duty Vehicles, though July 17, 2024')
 #col1, col2 = st.columns(2)
 
 #with col1:
-    # Read and filter the dataframe
-    df = pd.read_csv('https://github.com/mike-ua/Streamlit-Data/blob/main/light-duty-vehicles-2024-07-17.csv?raw=true')
-    df = df[df['Fuel_Code'].isin(['ELEC', 'HYBR', 'PHEV'])]
+# Read and filter the dataframe
+df = pd.read_csv('https://github.com/mike-ua/Streamlit-Data/blob/main/light-duty-vehicles-2024-07-17.csv?raw=true')
+df = df[df['Fuel_Code'].isin(['ELEC', 'HYBR', 'PHEV'])]
 
-    # Convert 'Model_Year' to string for display
-    df['Model_Year'] = df['Model_Year'].astype(str)
+# Convert 'Model_Year' to string for display
+df['Model_Year'] = df['Model_Year'].astype(str)
 
-    # Group by 'Model_Year' and 'Fuel_Code', then count and reshape
-    grouped_df = df.groupby(['Model_Year', 'Fuel_Code']).size()
-    reshaped_df = grouped_df.unstack(level='Fuel_Code')
+# Group by 'Model_Year' and 'Fuel_Code', then count and reshape
+grouped_df = df.groupby(['Model_Year', 'Fuel_Code']).size()
+reshaped_df = grouped_df.unstack(level='Fuel_Code')
 
-    # Display the DataFrame with formatted 'Model_Year' column
-    st.dataframe(reshaped_df)
+# Display the DataFrame with formatted 'Model_Year' column
+st.dataframe(reshaped_df)
 
 #with col2:
-    # Convert 'Model_Year' to numeric, round, and then 'Int64'
-    df['Model_Year'] = pd.to_numeric(df['Model_Year'], errors='coerce').astype('Int64')
+# Convert 'Model_Year' to numeric, round, and then 'Int64'
+df['Model_Year'] = pd.to_numeric(df['Model_Year'], errors='coerce').astype('Int64')
 
-    # Group by 'Model_Year' and 'Fuel_Code', then sum
-    line_chart_df = df.groupby(['Model_Year', 'Fuel_Code']).size().unstack(level='Fuel_Code').fillna(0)
+# Group by 'Model_Year' and 'Fuel_Code', then sum
+line_chart_df = df.groupby(['Model_Year', 'Fuel_Code']).size().unstack(level='Fuel_Code').fillna(0)
 
-    # Create a Plotly line chart
-    fig = px.line(line_chart_df, x=line_chart_df.index, y=line_chart_df.columns, title='Total Models of Electric Cars')
+# Create a Plotly line chart
+fig = px.line(line_chart_df, x=line_chart_df.index, y=line_chart_df.columns, title='Total Models of Electric Cars')
 
-    # Display the Plotly figure
-    st.plotly_chart(fig)
+# Display the Plotly figure
+st.plotly_chart(fig)
 
 st.write('source: https://afdc.energy.gov/')
